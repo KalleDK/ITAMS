cb3b1ed192e87a883eb42010abbf832937c31ce0
Kalle R. Møller <git@k-moeller.dk>
2017-03-06 14:18:14 +0100

    Moving Square

--- EmbeddedSnake/EmbeddedSnake/screenbuffer.h
+++ EmbeddedSnake/EmbeddedSnake/screenbuffer.h
@@ -9,11 +9,12 @@
 #ifndef SCREENBUFFER_H_
 #define SCREENBUFFER_H_
 
+#include <assert.h>
 #include <string.h>
 
 struct Point {
-	int8_t x;
-	int8_t y;
+	int16_t x;
+	int16_t y;
 };
 
 template<uint8_t WIDTH, uint8_t HEIGHT, typename DISPLAY>
@@ -35,8 +36,18 @@ struct ScreenBuffer
 		display->Write(buffer, size);
 	}
 	
-	void clear() {
-		memset(buffer, size);
+	void update(Point point, uint8_t width, uint8_t height) {
+		
+		uint8_t row = (point.y/8 < 0 ? 0 : point.y/8);
+		//uint8_t row_height = 2;
+		uint8_t row_height = ((point.y+height+7)/8 > rows ? rows : (point.y+height+7)/8) - row;
+		
+		for (uint8_t column = point.x; column < point.x+width; ++column) { 
+			display->SetYAddress(row);
+			display->SetXAddress(column);
+			uint8_t* ptr = &buffer[column*rows+row];
+			display->Write(ptr, row_height);
+		}
 	}
 	
 	bool is_inside (const Point& point, uint8_t width, uint8_t height) {
@@ -109,6 +120,80 @@ struct ScreenBuffer
 			height -= middle_height;
 			point.y += middle_height;
 		}
+	}
+	
+	void set(uint8_t data ) {
+		memset(buffer, data, size);
+	}
+	
+	void clear() {
+		set(0x00);
+	}
+	
+	void set() {
+		set(0xFF);
+	}
+	
+	void set(Point point) {
+		draw_square(point, 1, 1);		
+	}
+	
+	void clear(Point point) {
+		clear_square(point, 1, 1);
+	}
+	
+	void clear_square(Point point, uint8_t width, uint8_t height) {
+		if (!is_inside(point, width, height)) {
+			return;
+		}
+		
+		// Moving x into the screen an makes the width smaller
+		if (point.x < 0) {
+			width += point.x;
+			point.x = 0;
+		}
+		
+		// Moving y into the screen and makes height smaller
+		if (point.y < 0) {
+			height += point.y;
+			point.y = 0;
+		}
+		
+		// The squares goes out to the bottom, so we make it smaller
+		if ((point.y + height) > (rows * 8)) {
+			height = (rows * 8) - point.y;
+		}
+		
+		// The box goes out to the right, so we make it smaller
+		if ((point.x + width) > columns) {
+			width = columns - point.x;
+		}
+		
+		
+		while (height > 0) {
+			uint8_t bottom_height = point.y % 8;
+			uint8_t middle_height = (bottom_height + height > 7? 8-bottom_height: height);
+			uint8_t top_height = (8 - middle_height - bottom_height);
+			
+			uint8_t row = point.y / 8;
+			uint8_t mask = ~(0x00) << (bottom_height + top_height);
+			mask = mask >> top_height;
+			uint8_t* ptr = &buffer[rows*point.x+row];
+			
+			for (auto x = point.x; x < point.x + width; ++x) {
+				*ptr &= ~mask;
+				ptr += rows;
+			}
+			height -= middle_height;
+			point.y += middle_height;
+		}
+	}
+	
+	void draw_border(Point point, uint8_t width, uint8_t height, uint8_t thickness) {
+		draw_square(point, width, thickness);
+		draw_square(point, thickness, height);
+		draw_square(Point{point.x, point.y+height-thickness}, width, thickness);
+		draw_square(Point{point.x+width-thickness, point.y}, thickness, height);
 	}
 };
 
