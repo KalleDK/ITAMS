af93811e23650fbf2ee9717a3a39594e20f8690d
Kalle R. Møller <git@k-moeller.dk>
2017-03-06 08:44:13 +0100

    Better pins

--- EmbeddedSnake/EmbeddedSnake/snakeio.h
+++ EmbeddedSnake/EmbeddedSnake/snakeio.h
@@ -12,14 +12,13 @@
 #define SNAKEIO_H_
 
 // NES Controller setup
-using NES_DataPin = Pin<PortD, 0>;
-using NES_ClockPin = Pin<PortD, 1>;
-using NES_LatchPin = Pin<PortD, 7>;
+using NES_DataPin = Pin<PortD, PD0>;
+using NES_ClockPin = Pin<PortD, PD1>;
+using NES_LatchPin = Pin<PortD, PD7>;
 
 using SnakeController = NESController<NES_DataPin, NES_LatchPin, NES_ClockPin>;
 
 using Leds = PortC;
-
 
 
 #endif /* SNAKEIO_H_ */
